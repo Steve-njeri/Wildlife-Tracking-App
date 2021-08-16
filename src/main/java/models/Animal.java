d5de6732cc47cb157fb05b6487d8851c7dd6e067
Steve-njeri <stevenjeri254@gmail.com>
2021-08-16 15:32:34 +0300

    added first test and set up a new variable

--- src/main/java/models/Animal.java
+++ src/main/java/models/Animal.java
@@ -1,4 +1,9 @@
 package models;
 
 public class Animal {
+    private int id;
+    private String name;
+
+    public Animal(String name) {
+    }
 }
--- src/test/java/models/AnimalTest.java
+++ src/test/java/models/AnimalTest.java
@@ -1,7 +1,15 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class AnimalTest {
+
+    @Test
+    public void animal_instantiatesCorrectly_true() {
+        Animal animal = new Animal("Leopard");
+        assertEquals(true, animal instanceof Animal);
+    }
 
 }
