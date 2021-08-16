32cde07f7170ea218434418374d7357e0b5ef95d
Steve-njeri <stevenjeri254@gmail.com>
2021-08-16 17:26:08 +0300

    added tests that ensure Endangered Animal class getter methods return correctly

--- src/main/java/models/EndangeredAnimal.java
+++ src/main/java/models/EndangeredAnimal.java
@@ -6,5 +6,20 @@ public class EndangeredAnimal {
     private String age;
 
     public EndangeredAnimal(String name, String health, String age) {
+        this.name = name;
+        this.health = health;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public String getAge() {
+        return age;
     }
 }
--- src/test/java/models/EndangeredAnimalTest.java
+++ src/test/java/models/EndangeredAnimalTest.java
@@ -8,8 +8,26 @@ public class EndangeredAnimalTest {
 
     @Test
     public void endangeredAnimalObjectsInstantiateCorrectly_true() {
-        EndangeredAnimal endangeredAnimal = new EndangeredAnimal("Antelope", "healthy", "Young");
+        EndangeredAnimal endangeredAnimal = new EndangeredAnimal("Antelope", "Healthy", "Young");
         assertEquals(true, endangeredAnimal instanceof EndangeredAnimal);
+    }
+
+    @Test
+    public void endangeredAnimalNameReturnsCorrectly_String() {
+        EndangeredAnimal endangeredAnimal = new EndangeredAnimal("Antelope", "Healthy", "Young");
+        assertEquals("Antelope", endangeredAnimal.getName());
+    }
+
+    @Test
+    public void endangeredAnimalHealthStatusReturnsCorrectly_int() {
+        EndangeredAnimal endangeredAnimal = new EndangeredAnimal("Antelope", "Healthy", "Young");
+        assertEquals("Healthy", endangeredAnimal.getHealth());
+    }
+
+    @Test
+    public void endangeredAnimalAgeReturnsCorrectly_String() {
+        EndangeredAnimal endangeredAnimal = new EndangeredAnimal("Antelope", "Healthy", "Young");
+        assertEquals("Young", endangeredAnimal.getAge());
     }
 
 }
