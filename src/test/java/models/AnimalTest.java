02c93f1c5a166ec26645f5f2ce14141e5f48303b
Steve-njeri <stevenjeri254@gmail.com>
2021-08-16 16:09:44 +0300

    added the rule to AnimalTest class

--- src/test/java/models/AnimalTest.java
+++ src/test/java/models/AnimalTest.java
@@ -1,10 +1,14 @@
 package models;
 
+import org.junit.Rule;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class AnimalTest {
+
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
 
     @Test
     public void animal_instantiatesCorrectly_true() {
