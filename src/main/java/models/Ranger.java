7fcbbc01e8a572e72ea1e67f958bd511f6ca99f0
Steve-njeri <stevenjeri254@gmail.com>
2021-08-16 23:24:18 +0300

    added tests that ensure Ranger class getter methods return correctly

--- src/main/java/models/Ranger.java
+++ src/main/java/models/Ranger.java
@@ -7,10 +7,26 @@ public class Ranger {
     private int phone_number;
     private String email;
 
-    public Ranger(String name, int tag_number, int mobile_number, String email) {
+    public Ranger(String name, int badge_number, int phone_number, String email) {
         this.name = name;
-        this.badge_number = tag_number;
-        this.phone_number = mobile_number;
+        this.badge_number = badge_number;
+        this. phone_number = phone_number;
         this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBadge_number() {
+        return badge_number;
+    }
+
+    public int getPhone_number() {
+        return phone_number;
+    }
+
+    public String getEmail() {
+        return email;
     }
 }
--- src/test/java/models/RangerTest.java
+++ src/test/java/models/RangerTest.java
@@ -9,8 +9,32 @@ public class RangerTest {
     @Test
     public void instantiatesRangerObjectsCorrectly_true() throws Exception{
         Ranger ranger = new Ranger("Stephen", 20123, 0700000000, "stevenjeri254@gmail.com" );
-        assertEquals(true, true);
+        assertTrue(ranger instanceof Ranger);
 
+    }
+
+    @Test
+    public void rangerNameReturnsCorrectly_String() throws Exception{
+        Ranger ranger = new Ranger("Stephen", 20123, 0700000000, "stevenjeri254@gmail.com" );
+        assertEquals("Stephen",ranger.getName());
+    }
+
+    @Test
+    public void rangerTagNumberReturnsCorrectly_String() {
+        Ranger ranger = new Ranger("Stephen", 20123, 0700000000, "stevenjeri254@gmail.com" );
+        assertEquals(20123,ranger.getBadge_number());
+    }
+
+    @Test
+    public void rangerMobileNumberReturnsCorrectly_int() throws Exception{
+        Ranger ranger = new Ranger("Stephen", 20123, 0700000000, "stevenjeri254@gmail.com" );
+        assertEquals(0700000000,ranger.getPhone_number());
+    }
+
+    @Test
+    public void rangerEmailReturnsCorrectly_String() throws Exception{
+        Ranger ranger = new Ranger("Stephen", 20123, 0700000000, "stevenjeri254@gmail.com" );
+        assertEquals("stevenjeri254@gmail.com",ranger.getEmail());
     }
 
 }
