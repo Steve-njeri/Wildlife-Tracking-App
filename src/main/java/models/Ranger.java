334708dbf77505afde41c751c2afde17eaae367a
Steve-njeri <stevenjeri254@gmail.com>
2021-08-16 23:14:46 +0300

    added member variables and a public constructor in Ranger class

--- src/main/java/models/Ranger.java
+++ src/main/java/models/Ranger.java
@@ -1,5 +1,16 @@
 package models;
 
 public class Ranger {
+    private int id;
+    private String name;
+    private int badge_number;
+    private int phone_number;
+    private String email;
 
+    public Ranger(String name, int tag_number, int mobile_number, String email) {
+        this.name = name;
+        this.badge_number = tag_number;
+        this.phone_number = mobile_number;
+        this.email = email;
+    }
 }
--- src/test/java/models/RangerTest.java
+++ src/test/java/models/RangerTest.java
@@ -1,7 +1,16 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class RangerTest {
+
+    @Test
+    public void instantiatesRangerObjectsCorrectly_true() throws Exception{
+        Ranger ranger = new Ranger("Stephen", 20123, 0700000000, "stevenjeri254@gmail.com" );
+        assertEquals(true, true);
+
+    }
 
 }
